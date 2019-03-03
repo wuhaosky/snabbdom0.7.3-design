12a11850fc0b5a9fe949d873e648f6abf5742978
wuhaosky <wuhaosky@163.com>
2019-03-03 19:48:21 +0800

    updateChildren

--- src/snabbdom.ts
+++ src/snabbdom.ts
@@ -210,8 +210,10 @@ export function init(modules: Array<Partial<Module>>, domApi?: DOMAPI) {
     let idxInOld: number;
     let elmToMove: VNode;
     let before: any;
-
+    // 遍历 oldCh 和 newCh 来比较和更新
     while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+      // 一 首先检查 4 种情况，保证 oldStart/oldEnd/newStart/newEnd 这 4 个 vnode 非空
+      // 如果左侧的 vnode 为空就右移下标，如果右侧的 vnode 为空就左移 下标。
       if (oldStartVnode == null) {
         oldStartVnode = oldCh[++oldStartIdx]; // Vnode might have been moved left
       } else if (oldEndVnode == null) {
@@ -220,37 +222,80 @@ export function init(modules: Array<Partial<Module>>, domApi?: DOMAPI) {
         newStartVnode = newCh[++newStartIdx];
       } else if (newEndVnode == null) {
         newEndVnode = newCh[--newEndIdx];
-      } else if (sameVnode(oldStartVnode, newStartVnode)) {
+      } 
+      // 二 然后 oldStartVnode/oldEndVnode/newStartVnode/newEndVnode 两两比较，
+      //  * 对有相同 vnode 的 4 种情况执行对应的 patch 逻辑。
+      //  * - 如果同 start 或同 end 的两个 vnode 是相同的（情况 1 和 2），
+      //  *   说明不用移动实际 dom，直接更新 dom 属性／children 即可；
+      //  * - 如果 start 和 end 两个 vnode 相同（情况 3 和 4），
+      //  *   那说明发生了 vnode 的移动，同理我们也要移动 dom。
+      // 1. 如果 oldStartVnode 和 newStartVnode 相同（key相同），执行 patch
+      else if (sameVnode(oldStartVnode, newStartVnode)) {
+        // 不需要移动 dom
         patchVnode(oldStartVnode, newStartVnode, insertedVnodeQueue);
         oldStartVnode = oldCh[++oldStartIdx];
         newStartVnode = newCh[++newStartIdx];
-      } else if (sameVnode(oldEndVnode, newEndVnode)) {
+      } 
+      // 2. 如果 oldEndVnode 和 newEndVnode 相同，执行 patch
+      else if (sameVnode(oldEndVnode, newEndVnode)) {
+        // 不需要移动 dom
         patchVnode(oldEndVnode, newEndVnode, insertedVnodeQueue);
         oldEndVnode = oldCh[--oldEndIdx];
         newEndVnode = newCh[--newEndIdx];
-      } else if (sameVnode(oldStartVnode, newEndVnode)) { // Vnode moved right
+      } 
+      // 3. 如果 oldStartVnode 和 newEndVnode 相同，执行 patch
+      else if (sameVnode(oldStartVnode, newEndVnode)) { // Vnode moved right
         patchVnode(oldStartVnode, newEndVnode, insertedVnodeQueue);
+        // 把获得更新后的 (oldStartVnode/newEndVnode) 的 dom 右移，移动到
+        // oldEndVnode 对应的 dom 的右边。为什么这么右移？
+        // （1）oldStartVnode 和 newEndVnode 相同，显然是 vnode 右移了。
+        // （2）若 while 循环刚开始，那移到 oldEndVnode.elm 右边就是最右边，是合理的；
+        // （3）若循环不是刚开始，因为比较过程是两头向中间，那么两头的 dom 的位置已经是
+        //     合理的了，移动到 oldEndVnode.elm 右边是正确的位置；
+        // （4）记住，oldVnode 和 vnode 是相同的才 patch，且 oldVnode 自己对应的 dom
+        //     总是已经存在的，vnode 的 dom 是不存在的，直接复用 oldVnode 对应的 dom。
         api.insertBefore(parentElm, oldStartVnode.elm as Node, api.nextSibling(oldEndVnode.elm as Node));
         oldStartVnode = oldCh[++oldStartIdx];
         newEndVnode = newCh[--newEndIdx];
-      } else if (sameVnode(oldEndVnode, newStartVnode)) { // Vnode moved left
+      } 
+      // 4. 如果 oldEndVnode 和 newStartVnode 相同，执行 patch
+      else if (sameVnode(oldEndVnode, newStartVnode)) { // Vnode moved left
         patchVnode(oldEndVnode, newStartVnode, insertedVnodeQueue);
+        // 这里是左移更新后的 dom，原因参考上面的右移。
         api.insertBefore(parentElm, oldEndVnode.elm as Node, oldStartVnode.elm as Node);
         oldEndVnode = oldCh[--oldEndIdx];
         newStartVnode = newCh[++newStartIdx];
-      } else {
+      } 
+      // 三 最后一种情况：4 个 vnode 都不相同，那么我们就要
+      // 1. 从 oldCh 数组建立 key --> index 的 map。
+      // 2. 只处理 newStartVnode （简化逻辑，有循环我们最终还是会处理到所有 vnode），
+      //    以它的 key 从上面的 map 里拿到 index；
+      // 3. 如果 index 存在，那么说明有对应的 old vnode，patch 就好了；
+      // 4. 如果 index 不存在，那么说明 newStartVnode 是全新的 vnode，直接
+      //    创建对应的 dom 并插入。
+      else {
+        // 如果 oldKeyToIdx 不存在，创建 old children 中 vnode 的 key 到 index 的
+        // 映射，方便我们之后通过 key 去拿下标。
         if (oldKeyToIdx === undefined) {
           oldKeyToIdx = createKeyToOldIdx(oldCh, oldStartIdx, oldEndIdx);
         }
+        // 尝试通过 newStartVnode 的 key 去拿下标
         idxInOld = oldKeyToIdx[newStartVnode.key as string];
+        // 下标不存在，说明 newStartVnode 是全新的 vnode。
         if (isUndef(idxInOld)) { // New element
+          // 那么为 newStartVnode 创建 dom 并插入到 oldStartVnode.elm 的前面。
           api.insertBefore(parentElm, createElm(newStartVnode, insertedVnodeQueue), oldStartVnode.elm as Node);
           newStartVnode = newCh[++newStartIdx];
-        } else {
+        } 
+        // 下标存在，说明 old children 中有相同 key 的 vnode，
+        else {
           elmToMove = oldCh[idxInOld];
+          // 如果 type 不同，没办法，只能创建新 dom；
           if (elmToMove.sel !== newStartVnode.sel) {
             api.insertBefore(parentElm, createElm(newStartVnode, insertedVnodeQueue), oldStartVnode.elm as Node);
-          } else {
+          } 
+          // type 相同（且key相同），那么说明是相同的 vnode，执行 patch。
+          else {
             patchVnode(elmToMove, newStartVnode, insertedVnodeQueue);
             oldCh[idxInOld] = undefined as any;
             api.insertBefore(parentElm, (elmToMove.elm as Node), oldStartVnode.elm as Node);
@@ -259,11 +304,16 @@ export function init(modules: Array<Partial<Module>>, domApi?: DOMAPI) {
         }
       }
     }
+    // 上面的循环结束后（循环条件有两个），处理可能的未处理到的 vnode。
+    // 如果是 new vnodes 里有未处理的（oldStartIdx > oldEndIdx
+    // 说明 old vnodes 先处理完毕）
     if (oldStartIdx <= oldEndIdx || newStartIdx <= newEndIdx) {
       if (oldStartIdx > oldEndIdx) {
         before = newCh[newEndIdx+1] == null ? null : newCh[newEndIdx+1].elm;
         addVnodes(parentElm, before, newCh, newStartIdx, newEndIdx, insertedVnodeQueue);
-      } else {
+      } 
+      // 相反，如果 old vnodes 有未处理的，删除 （为处理 vnodes 对应的） 多余的 dom。
+      else {
         removeVnodes(parentElm, oldCh, oldStartIdx, oldEndIdx);
       }
     }
